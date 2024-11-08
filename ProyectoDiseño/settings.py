f0cf492bb830513de76a361996c5462b345c43ee
Reeddox <oscaracevedosanchez@gmail.com>
2024-11-08 20:05:17 -0300

    Update settings.py

--- ProyectoDiseño/settings.py
+++ ProyectoDiseño/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8per^$a19+y=ud22=hy5g79-)4znf&5ux5ggbxhf9paem)59uj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
